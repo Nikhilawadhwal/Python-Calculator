03c93026b4e84c9686cb20410fb5aaf812b84d0d
Nikhil Awadhwal <nikhilawadhwal2@gmail.com>
2026-02-01 19:48:07 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,24 @@
+print("CALCULATER")
+
+a = int(input("Enter The First Number = " ))
+
+b = int(input("Enter The Secound Number = " ))
+
+operator = input("Enter the operator = ")
+
+if operator == "+":
+    add = a+b
+    print("the sum of the number is = ",add)
+if operator == "-":
+    sub = a-b
+    print("the subraction of the number is = ",sub) 
+if operator == "*":
+    mul = a*b
+    print("the multiplication of the number is = ",mul)
+if operator == "/":
+    div = a/b
+    print("the division of the number is = ",div)
+if operator == "%":
+    mod = a%b
+    print("the modules of the number is = ",mod)
+
